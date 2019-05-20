71f9d5f46fbc6d6744ec573277f9190d53d6e9a1
julia gandit <julia.gandit@gmail.com>
2019-05-20 16:03:01 +0200

    added pet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_134014) do
+ActiveRecord::Schema.define(version: 2019_05_20_135648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.text "bio"
+    t.integer "age"
+    t.integer "price"
+    t.bigint "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_pets_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_05_20_134014) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users", column: "owner_id"
 end
