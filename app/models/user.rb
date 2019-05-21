9885f791643364068f15add207c588e758a8bfc1
Armen <armen_bagramian@gmail.com>
2019-05-21 18:54:41 +0200

    adding awesome feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :pets, foreign_key: "owner_id"
   has_many :bookings, foreign_key: "sitter_id"
   has_many :booked_pets, through: :bookings, source: "pet"
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
   # validates :name, presence: true, uniqueness: true
   # validates :email, presence: true
 end
