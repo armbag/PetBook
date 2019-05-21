7746bf78f37e6d7f52ec0dd541565999fb2d18df
Armen <armen_bagramian@gmail.com>
2019-05-21 11:13:04 +0200

    to complete

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,34 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show]
+
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.new
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pets_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :price, :species, :bio, :age)
+  end
 end
