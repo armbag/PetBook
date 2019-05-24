d1cd32e8641932d34fa4cabadcb9d1fa39a6fd68
Armen <armen_bagramian@gmail.com>
2019-05-24 12:15:41 +0200

    add migration available

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pets, only: [:index, :show, :new, :create, :destroy] do
-   resources :bookings, only: [:new, :create, :index, :show]
+    resources :bookings, only: [:new, :create, :index, :show]
   end
   resources :bookings, only: [:destroy]
 end
